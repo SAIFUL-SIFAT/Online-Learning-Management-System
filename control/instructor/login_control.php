eef797c4c83032c83db4388282b1df115a2a1eb2
Sifat <sifat.sai3@gmail.com>
2025-01-22 20:42:42 +0600

    Updated login_control and signup functionality

--- control/instructor/login_control.php
+++ control/instructor/login_control.php
@@ -4,7 +4,7 @@ session_start();
 $_SESSION['full_name'] = $_POST['full_name'];
 $_SESSION['email'] = $_POST['email'];
 
-require('../model/db.php');
+require('../../model/instructor/db.php');
 
 if (!isset($_POST['full_name']) || !isset($_POST['pass'])) {
     die('All fields are required!');
@@ -21,4 +21,4 @@ if ($result == null) {
 unset($result['pass']);
 $_SESSION = $result;
 
-header('location: ../../view/instructor/dashboard.php');
+header('location:../../view/instructor/dashboard.php');
